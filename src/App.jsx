299e1dcd43715170908f684969b340cd218d39b0
Reforms <reformsgames@gmail.com>
2022-11-28 23:56:01 +0800

    fix starter file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-
-import { BrowserRouter, Routes, Route  } from 'react-dom'
+import React from 'react'
+import { BrowserRouter, Routes, Route  } from 'react-router-dom'
 import Home from "./pages/Home"
-import NotFound from './pages/NotFound';
+import NotFound from './pages/NotFound'
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default App
